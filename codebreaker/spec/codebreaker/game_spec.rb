8b8e141be4d39f9f5c7637cea1f71b18cb814374
Jeff Powell <powellj@mac.com>
2010-12-25 02:12:37 -0700

    5.3 - Spec tests passing, time to refactor

--- codebreaker/spec/codebreaker/game_spec.rb
+++ codebreaker/spec/codebreaker/game_spec.rb
@@ -4,17 +4,22 @@ module Codebreaker
   describe Game do
     describe "#start" do
       it "sends a welcome message" do
-        output = double('output')
-        game = Game.new(output)
-        
+        output = double('output').as_null_object
+        game = Game.new(output)        
         output.should_receive(:puts).with('Welcome to Codebreaker!')      
         game.start
       end
-      it "prompts for the first guess"
+      
+      it "prompts for the first guess" do
+        output = double('output').as_null_object
+        game = Game.new(output)        
+        output.should_receive(:puts).with('Enter guess:')        
+        game.start
+      end
+      
     end
   end
 end
-
 
 # decribe returns a subclass of RSpec::Core::ExampleGroup (a group of examples of expected behavior of an object - similar to Test::Unit's TestCase)
 # it() method creates an example that is an instance of the ExampleGroup returned by describe()
